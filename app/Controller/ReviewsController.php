00e23821992cfb01cfb1d43f34b204156587c21d
professir <buckcyoung@gmail.com>
2014-03-26 19:16:30 -0400

    AutoCommit

--- app/Controller/ReviewsController.php
+++ app/Controller/ReviewsController.php
@@ -13,24 +13,14 @@ class ReviewsController extends AppController{
 	}
 	
 	public function add(){
-		$userid = $this->Auth->user('id');
-		if ( !$userid ){ //not set
-
-			$this->Session->setFlash(__('You must be logged in to add a review.'));
-			return $this->redirect(array('action' => 'index')); //reload page to show flash
-
-		} else {
-
-			if( $this->request->is('post') ){
-				//save data
-				$this->request->data['Review']['user_id'] = $userid;
-				$this->Review->create(); //????????
-				$this->Review->save( $this->request->data );
-				
-				//Redirect -- BUG: will redirect before validating data
-				//$this->redirect('/reviews/');
-			}
-
+		if( $this->request->is('post') ){
+			//save data
+			$this->request->data['Review']['user_id'] = $this->Auth->user('id');
+			///$this->Review->create(); //????????
+			$this->Review->save( $this->request->data );
+			
+			//Redirect -- BUG: will redirect before validating data
+			//$this->redirect('/reviews/');
 		}
 	}
 }
