9f9845029ef7effa1e057b47e59c967d59b367cb
professir <buckcyoung@gmail.com>
2014-03-27 19:59:32 -0400

    auto

--- app/Controller/CommentsController.php
+++ app/Controller/CommentsController.php
@@ -6,7 +6,7 @@ class CommentsController extends AppController{
 		if( $this->request->is('post') ){
 			//save data
 			$this->request->data['Comment']['review_id'] = $id; //Review id
-			$this->request->data['Comment']['user_id'] = $this->Auth->user('id'); //This is absolutely needed.
+			$this->request->data['Comment']['username'] = $this->Auth->user('username'); //This is absolutely needed.
 			if ($this->Comment->save( $this->request->data ) ){
 				$this->Session->setFlash(__('Comment created.'));
                 return $this->redirect(array('controller' => 'reviews','action' => 'view', $id)); //go back to the view id we were on!
