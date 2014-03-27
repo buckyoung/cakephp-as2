62f7c92c7149390307020d040c4a51a5a25f028a
professir <buckcyoung@gmail.com>
2014-03-27 02:02:12 -0400

    AutoCommit

--- app/Controller/CommentsController.php
+++ app/Controller/CommentsController.php
@@ -6,6 +6,7 @@ class CommentsController extends AppController{
 		if( $this->request->is('post') ){
 			//save data
 			$this->request->data['Comment']['review_id'] = $id; //Review id
+			$this->request->data['Comment']['from'] = $this->Review->user('id'); //This is absolutely needed.
 			if ($this->Comment->save( $this->request->data ) ){
 				$this->Session->setFlash(__('Comment created.'));
                 return $this->redirect(array('controller' => 'reviews','action' => 'view', $id)); //go back to the view id we were on!
