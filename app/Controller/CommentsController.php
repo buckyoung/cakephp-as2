8bba859310a64a26e4f26dfe3a7676ad5d61a728
professir <buckcyoung@gmail.com>
2014-03-27 20:57:51 -0400

    auto

--- app/Controller/CommentsController.php
+++ app/Controller/CommentsController.php
@@ -26,7 +26,7 @@ class CommentsController extends AppController{
 
 	public function edit($id, $review_id){
 
-		$this->request->data = $this->Comments->findById($id);
+		$this->request->data = $this->Comment->findById($id);
 
 		if( $this->request->is('post') ){
 			
