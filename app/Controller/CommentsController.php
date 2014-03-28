22d7df8a9e32f5bb8f7ceb9d8a47ea091493ed7f
professir <buckcyoung@gmail.com>
2014-03-28 02:38:51 -0400

    auto

--- app/Controller/CommentsController.php
+++ app/Controller/CommentsController.php
@@ -29,10 +29,9 @@ class CommentsController extends AppController{
 
 		if( $this->request->is(array('post', 'put')) ){
 			//save data
-			//$this->Comment->id = $id;
-			//$this->request->data['Comment']['id'] = $id; //Comment id
-			//$this->request->data['Comment']['review_id'] = $review_id; //Review id
-			//$this->request->data['Comment']['username'] = $this->Auth->user('username'); //This is absolutely needed.
+			$this->request->data['Comment']['id'] = $id; //Comment id
+			$this->request->data['Comment']['review_id'] = $review_id; //Review id
+			$this->request->data['Comment']['username'] = $this->Auth->user('username'); //This is absolutely needed.
 
 			if ($this->Comment->save( $this->request->data ) ){
 				$this->Session->setFlash(__('Comment Edited.'));
